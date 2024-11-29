7dbf5388e317aa4f0e7ea76282fe11b87c377be8
Denis mgaya <dnspascal094@gmail.com>
2024-11-29 16:05:31 +0300

    Removal of unused imports in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:get/get.dart';
 import 'package:travel/app.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:travel/core/controllers/locale_controller.dart';
 
 void main() async {
   await GetStorage.init();
