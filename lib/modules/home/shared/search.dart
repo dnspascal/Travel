ff7bb6471304cacaa20a962ddbc617d0aba81d81
Denis mgaya <dnspascal094@gmail.com>
2024-11-29 11:22:03 +0300

    Code arrangement

--- lib/modules/home/shared/search.dart
+++ lib/modules/home/shared/search.dart
@@ -1,6 +1,6 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:get/get.dart';
 import 'package:travel/core/controllers/home_controller.dart';
 
 class SearchSection extends GetView<HomeController> {
